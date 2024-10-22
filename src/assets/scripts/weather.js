1f6a4a7c49fa5cc033908b6d5e50d4ffd61e668d
dlgnsrb227 <125418331+dlgnsrb227@users.noreply.github.com>
2024-10-22 13:33:11 +0900

    console

--- src/assets/scripts/weather.js
+++ src/assets/scripts/weather.js
@@ -82,8 +82,8 @@ export async function getWeather() {
       }
     );
 
-    console.log(`response : ${response}`);
-    console.log(`body : ${response.data.response.body}`);
+    console.log(response);
+    console.log(response.data.response.body);
 
     if (response && response.data.response.body) {
       const weatherInfo = response.data.response.body.items.item;
